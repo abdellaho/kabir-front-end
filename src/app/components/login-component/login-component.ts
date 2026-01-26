bfc90f8813eb265c1153f0dffb621db6d3761ab4
abdellaho <bojaneabdellaho@gmail.com>
2026-01-26 15:39:28 +0100

    edit in login, errors

--- src/app/components/login-component/login-component.ts
+++ src/app/components/login-component/login-component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AppFloatingConfigurator } from "@/layout/component/app.floatingconfigurator";
+import { AppFloatingConfigurator } from '@/layout/component/app.floatingconfigurator';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { PasswordModule } from "primeng/password";
+import { PasswordModule } from 'primeng/password';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
@@ -19,119 +19,107 @@ import { Erreur } from '@/shared/classes/erreur';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
-  selector: 'app-login-component',
-  imports: [
-    AppFloatingConfigurator, 
-    PasswordModule, 
-    CheckboxModule, 
-    InputTextModule,
-    ButtonModule, 
-    RippleModule,
-    SkeletonModule,
-    ToastModule,
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
-  templateUrl: './login-component.html',
-  styleUrl: './login-component.scss'
+    selector: 'app-login-component',
+    imports: [AppFloatingConfigurator, PasswordModule, CheckboxModule, InputTextModule, ButtonModule, RippleModule, SkeletonModule, ToastModule, CommonModule, ReactiveFormsModule, FormsModule],
+    templateUrl: './login-component.html',
+    styleUrl: './login-component.scss'
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
+    formGroup!: FormGroup;
+    adminExist: boolean = false;
+    loading: boolean = false;
 
-  formGroup!: FormGroup;
-  adminExist: boolean = false;
-  loading: boolean = false;
+    constructor(
+        private formBuilder: FormBuilder,
+        private personnelService: PersonnelService,
+        private stateService: StateService,
+        private router: Router,
+        private authService: AuthSecurityService,
+        private messageService: MessageService
+    ) {}
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private personnelService: PersonnelService,
-    private stateService: StateService,
-    private router: Router,
-    private authService: AuthSecurityService,
-    private messageService: MessageService
-  ) { }
-
-  async ngOnInit() {
-    this.loading = true; // Set loading to true at the start
-    try {
-      this.adminExist = await this.checkAdminExist();
-    } catch (error) {
-      console.error('Error during init:', error);
-      this.adminExist = false; // Fallback value
-    } finally {
-      this.initForm();
-      this.loading = false; // Always set to false after completion
-    }
-  }
-
-  async checkAdminExist(): Promise<boolean> {
-    try {
-        const existsObservable = this.personnelService.adminExist().pipe(
-            catchError(error => {
-                console.error('Error in personnel existence observable:', error);
-                return of(false); // Gracefully handle observable errors by returning false
-            })
-        );
-        return await firstValueFrom(existsObservable);
-    } catch (error) {
-        console.error('Unexpected error checking if personnel exists:', error);
-        return false;
-    }
-  }
-
-  initForm(){
-    this.formGroup = this.formBuilder.group({
-      email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required, Validators.minLength(1)]],
-      rememberMe: [false],
-    });
-  }
-
-  login() {
-    this.stateService.setState({ loading: true, error: null });
-
-    const authRequest: AuthRequest = {
-      email: this.formGroup.get('email')?.value,
-      password: this.formGroup.get('password')?.value
-    };
-    
-    if(this.adminExist) {
-      this.authService.login(authRequest).subscribe({
-        next: (response) => {
-          this.router.navigate(['/']);
-        },
-        error: (error: Erreur) => {
-          this.stateService.setState({ loading: false, error: error.message });
-          console.log('error', error);
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: error.message
-          });
-        }, 
-        complete: () => {
-          this.stateService.setState({ loading: false });
+    async ngOnInit() {
+        this.loading = true; // Set loading to true at the start
+        try {
+            this.adminExist = await this.checkAdminExist();
+        } catch (error) {
+            console.error('Error during init:', error);
+            this.adminExist = false; // Fallback value
+        } finally {
+            this.initForm();
+            this.loading = false; // Always set to false after completion
         }
-      });
-    } else {
-      this.authService.register(authRequest).subscribe({
-        next: (response) => {
-          this.router.navigate(['/']);
-        },
-        error: (error: Erreur) => {
-          this.stateService.setState({ loading: false, error: error.message });
-          console.log(error);
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: error.message
-          });
-        },
-        complete: () => {
-          this.stateService.setState({ loading: false });
-        }
-      });
     }
-  }
 
+    async checkAdminExist(): Promise<boolean> {
+        try {
+            const existsObservable = this.personnelService.adminExist().pipe(
+                catchError((error) => {
+                    console.error('Error in personnel existence observable:', error);
+                    return of(false); // Gracefully handle observable errors by returning false
+                })
+            );
+            return await firstValueFrom(existsObservable);
+        } catch (error) {
+            console.error('Unexpected error checking if personnel exists:', error);
+            return false;
+        }
+    }
+
+    initForm() {
+        this.formGroup = this.formBuilder.group({
+            email: [null, [Validators.required, Validators.email]],
+            password: [null, [Validators.required, Validators.minLength(1)]],
+            rememberMe: [false]
+        });
+    }
+
+    login() {
+        this.stateService.setState({ loading: true, error: null });
+
+        const authRequest: AuthRequest = {
+            email: this.formGroup.get('email')?.value,
+            password: this.formGroup.get('password')?.value
+        };
+
+        if (this.adminExist) {
+            this.authService.login(authRequest).subscribe({
+                next: (response) => {
+                    this.router.navigate(['/']);
+                },
+                error: (error: Erreur) => {
+                    //this.stateService.setState({ loading: false, error: error.message });
+                    console.log('state', this.stateService.getState());
+
+                    console.log('error', error);
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail: this.stateService.getState().error ? this.stateService.getState().error + '' : error.message
+                    });
+                },
+                complete: () => {
+                    this.stateService.setState({ loading: false });
+                }
+            });
+        } else {
+            this.authService.register(authRequest).subscribe({
+                next: (response) => {
+                    this.router.navigate(['/']);
+                },
+                error: (error: Erreur) => {
+                    //this.stateService.setState({ loading: false, error: error.message });
+                    console.log(error);
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail: error.message
+                    });
+                },
+                complete: () => {
+                    this.stateService.setState({ loading: false });
+                }
+            });
+        }
+    }
 }
