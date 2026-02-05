1273503f0ce4da7b335e35d6d19678f6d1e07f16
abdellaho <bojaneabdellaho@gmail.com>
2026-02-05 13:19:34 +0100

    change in parametrage component

--- src/app/components/parametrage-component/parametrage-component.ts
+++ src/app/components/parametrage-component/parametrage-component.ts
@@ -10,22 +10,18 @@ import { PrimeComponent } from '@/components/prime-component/prime-component';
 import { APP_MESSAGES } from '@/shared/classes/app-messages';
 
 @Component({
-  selector: 'app-parametrage-component',
-  imports: [
-      CommonModule,
-      TabsModule,
-      EtablissementComponent,
-      VilleComponent,
-      PersonnelComponent,
-      FournisseurComponent,
-      VoitureComponent,
-      PrimeComponent
-  ],
-  templateUrl: './parametrage-component.html',
-  styleUrl: './parametrage-component.scss'
+    selector: 'app-parametrage-component',
+    imports: [CommonModule, TabsModule, EtablissementComponent, VilleComponent, PersonnelComponent, FournisseurComponent, VoitureComponent, PrimeComponent],
+    templateUrl: './parametrage-component.html',
+    styleUrl: './parametrage-component.scss'
 })
 export class ParametrageComponent {
-
+    value: number = 0;
+    loadedTabs: Set<number> = new Set([0]);
     msg = APP_MESSAGES;
 
+    onTabChange(event: any) {
+        this.value = event;
+        this.loadedTabs.add(this.value);
+    }
 }
